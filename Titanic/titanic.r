99e1f6ee8c78b3d0b4a7a590c76bfbc74bf6f6f5
daisyworld2018 <39037277+daisyworld2018@users.noreply.github.com>
2018-09-06 22:47:00 -0700

    Update titanic.r

--- Titanic/titanic.r
+++ Titanic/titanic.r
@@ -2,8 +2,17 @@ titanic <- read.csv(file="titanic3.csv", header=TRUE, sep=",")
 library(dplyr)
 library(tidyr)
 
-titanic$embarked <- sub("^$", "S", titanic$embarked)
+#fill row with missing values with charater "S"
+titanic$embarked <- sub("^$", "S", titanic$embarked) 
+
+#calculate mean age for passengers
 age_mean <- mean(titanic$age,0,TRUE)
+
+#in age column replace rows with NULL values with mean age.
 titanic$age[is.na(titanic$age)] <- age_mean
+
+#replace rows with empty strings in boat column with charater "NA"
 titanic$boat <- sub("^$", "NA", titanic$boat)
+
+#create a new column has_cabin_number with boolean values based on whether cabin column has a cabin number or not
 titanic$has_cabin_number <- ifelse(titanic$cabin!="",1,0)
